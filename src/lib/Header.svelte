bd90a226611dd28b478c8b9ba3852c308189cee9
Tarek Siddique Nabil <86683713+Tarek-Siddique-Nabil@users.noreply.github.com>
2023-07-19 06:57:30 +0600

    initial commit

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -10,7 +10,10 @@
     <div class=" text-white flex-shrink-0">
       <h1 class="text-xl lg:text-4xl font-bold">Fooly Cooly</h1>
     </div>
-    <button class="text-white hover:text-slate-200 cursor-pointer lg:hidden" on:click={() => (navbar_show = true)}>
+    <button
+      class="text-white hover:text-slate-200 cursor-pointer lg:hidden"
+      on:click={() => (navbar_show = true)}
+    >
       <svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 448 512">
         <path
           fill="currentColor"
@@ -26,19 +29,41 @@
       <ul class="flex gap-8 text-white items-center lg:flex-row flex-col">
         {#each navbar_items_data as nav_data}
           <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
-          <li class="" on:keydown={null} on:click={() => (navbar_show = false)}><a href={nav_data.href} class="">{@html nav_data.label} </a></li>
+          <li class="" on:keydown={null} on:click={() => (navbar_show = false)}>
+            <a href={nav_data.href} class="">{@html nav_data.label} </a>
+          </li>
         {/each}
         <li>
-          <a href="https://twitter.com/FoolyCoolyERC" target="_blank" class="inline-block">
-            <svg class="inline-block mr-3" width="30" viewBox="0 0 36 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <a
+            href="https://twitter.com/FoolyCoolyERC"
+            target="_blank"
+            class="inline-block"
+          >
+            <svg
+              class="inline-block mr-3"
+              width="30"
+              viewBox="0 0 36 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M33.704 7.48979C33.3307 7.8015 33.1155 8.26761 33.1177 8.754C33.1181 8.84434 33.1181 8.93467 33.1181 9.025C33.1181 19.6999 25.2819 32 10.9594 32C7.21547 32 3.69722 31.046 0.610818 29.352C0.195319 29.124 0.406742 28.475 0.880711 28.475C2.34757 28.475 3.76749 28.268 5.11521 27.8787C7.14522 27.2921 6.50754 24.0942 5.06892 22.5465C4.58291 22.0236 4.16487 21.4331 3.82912 20.7899C3.51335 20.1849 4.0562 19.55 4.73862 19.55C5.26053 19.55 5.30456 18.8191 4.84156 18.5782C2.2894 17.2504 0.543088 14.5204 0.543088 11.35C0.543088 11.3056 0.591207 11.2778 0.629922 11.2995C0.958645 11.4841 1.30378 11.6462 1.66233 11.7827C2.33172 12.0375 2.72441 11.1518 2.28775 10.584C1.22468 9.20179 0.591331 7.45076 0.591331 5.54994C0.591331 3.52402 3.04987 3.2293 4.5763 4.56137C7.71338 7.299 11.5992 9.14417 15.8568 9.74451C16.7911 9.87625 17.5177 9.01855 17.5177 8.07501C17.5177 3.62496 20.9898 0 25.3058 0C27.2173 0 28.9712 0.708505 30.3284 1.89322C30.7662 2.27546 31.3472 2.48743 31.9089 2.33787C32.6364 2.14419 33.3473 1.89543 34.0329 1.59716C34.7169 1.29957 35.4394 1.91961 35.0395 2.54928C34.869 2.81787 34.6831 3.07534 34.4832 3.32041C34.0835 3.8103 34.4991 4.69456 35.104 4.51066C35.672 4.33798 36.1581 4.97913 35.7743 5.43203C35.1431 6.17687 34.4498 6.86718 33.704 7.48979Z"
                 fill="currentColor"
               />
             </svg>
           </a>
-          <a href="https://t.me/foolycoolyerc" target="_blank" class="inline-block ml-3">
-            <svg class="inline-block" width="30" viewBox="0 0 38 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <a
+            href="https://t.me/foolycoolyerc"
+            target="_blank"
+            class="inline-block ml-3"
+          >
+            <svg
+              class="inline-block"
+              width="30"
+              viewBox="0 0 38 33"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 fill-rule="evenodd"
                 clip-rule="evenodd"
@@ -48,8 +73,18 @@
             </svg>
           </a>
         </li>
-        <li><button class="px-5 py-2 bg-white text-black rounded">Buy Now</button></li>
-        <li class="block lg:hidden"><button class="px-5 py-2 border-white border-2 text-white rounded" on:click={() => (navbar_show = false)}>Back</button></li>
+        <li>
+          <a
+            href="https://app.uniswap.org/#/swap?outputCurrency=0xc65f8ce288954d914f58e3d19ea8294dfa5f2e5f"
+            class="px-5 py-2 bg-white text-black rounded">Buy Now</a
+          >
+        </li>
+        <li class="block lg:hidden">
+          <button
+            class="px-5 py-2 border-white border-2 text-white rounded"
+            on:click={() => (navbar_show = false)}>Back</button
+          >
+        </li>
       </ul>
     </div>
   </div>
@@ -59,18 +94,40 @@
   <div class="container">
     <div class="flex lg:flex-row flex-col lg:w-full lg:h-screen items-center">
       <div class="w-full lg:w-[45%] pt-28 px-5 text-center">
-        <h1 class="text-4xl lg:text-[78px] font-bold leading-[1.1em] mb-5 lg:text-left  heading-animation-character">Its time for FLCL to take on the World.</h1>
+        <h1
+          class="text-4xl lg:text-[78px] font-bold leading-[1.1em] mb-5 lg:text-left heading-animation-character"
+        >
+          Its time for FLCL to take on the World.
+        </h1>
         <div class="text-white text-center lg:text-left mb-8 lg:mb-0">
-          <a href="https://twitter.com/FoolyCoolyERC" target="_blank" class="inline-block">
-            <svg class="inline-block mr-3 w-[25px] lg:w-[40px]" viewBox="0 0 36 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <a
+            href="https://twitter.com/FoolyCoolyERC"
+            target="_blank"
+            class="inline-block"
+          >
+            <svg
+              class="inline-block mr-3 w-[25px] lg:w-[40px]"
+              viewBox="0 0 36 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M33.704 7.48979C33.3307 7.8015 33.1155 8.26761 33.1177 8.754C33.1181 8.84434 33.1181 8.93467 33.1181 9.025C33.1181 19.6999 25.2819 32 10.9594 32C7.21547 32 3.69722 31.046 0.610818 29.352C0.195319 29.124 0.406742 28.475 0.880711 28.475C2.34757 28.475 3.76749 28.268 5.11521 27.8787C7.14522 27.2921 6.50754 24.0942 5.06892 22.5465C4.58291 22.0236 4.16487 21.4331 3.82912 20.7899C3.51335 20.1849 4.0562 19.55 4.73862 19.55C5.26053 19.55 5.30456 18.8191 4.84156 18.5782C2.2894 17.2504 0.543088 14.5204 0.543088 11.35C0.543088 11.3056 0.591207 11.2778 0.629922 11.2995C0.958645 11.4841 1.30378 11.6462 1.66233 11.7827C2.33172 12.0375 2.72441 11.1518 2.28775 10.584C1.22468 9.20179 0.591331 7.45076 0.591331 5.54994C0.591331 3.52402 3.04987 3.2293 4.5763 4.56137C7.71338 7.299 11.5992 9.14417 15.8568 9.74451C16.7911 9.87625 17.5177 9.01855 17.5177 8.07501C17.5177 3.62496 20.9898 0 25.3058 0C27.2173 0 28.9712 0.708505 30.3284 1.89322C30.7662 2.27546 31.3472 2.48743 31.9089 2.33787C32.6364 2.14419 33.3473 1.89543 34.0329 1.59716C34.7169 1.29957 35.4394 1.91961 35.0395 2.54928C34.869 2.81787 34.6831 3.07534 34.4832 3.32041C34.0835 3.8103 34.4991 4.69456 35.104 4.51066C35.672 4.33798 36.1581 4.97913 35.7743 5.43203C35.1431 6.17687 34.4498 6.86718 33.704 7.48979Z"
                 fill="currentColor"
               />
             </svg>
           </a>
-          <a href="https://t.me/foolycoolyerc" target="_blank" class="inline-block ml-3">
-            <svg class="inline-block w-[25px] lg:w-[40px]" viewBox="0 0 38 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <a
+            href="https://t.me/foolycoolyerc"
+            target="_blank"
+            class="inline-block ml-3"
+          >
+            <svg
+              class="inline-block w-[25px] lg:w-[40px]"
+              viewBox="0 0 38 33"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 fill-rule="evenodd"
                 clip-rule="evenodd"
@@ -97,7 +154,13 @@
   }
   .heading-animation-character {
     text-transform: uppercase;
-    background-image: linear-gradient(-225deg,#ffffff 0%,#ffffff 29%,#D3D1A9 67%,#fff 100%);
+    background-image: linear-gradient(
+      -225deg,
+      #ffffff 0%,
+      #ffffff 29%,
+      #d3d1a9 67%,
+      #fff 100%
+    );
     background-size: auto auto;
     background-clip: border-box;
     background-size: 200% auto;
--- src/lib/navbar_items_data.ts
+++ src/lib/navbar_items_data.ts
@@ -16,12 +16,12 @@ export const navbar_items_data = [
   },
   {
     label: "DexTools",
-    href: "/#",
+    href: "https://www.dextools.io/app/en/ether/pair-explorer/0xe2b7bb4e5904637e5adaeaa50068ef39d2eaffda",
     is_button: false,
   },
   {
     label: "Etherscan",
-    href: "/#",
+    href: "https://etherscan.io/token/0xc65f8ce288954d914f58e3d19ea8294dfa5f2e5f",
     is_button: false,
   },
 ];
